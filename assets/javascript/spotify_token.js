95aa7d0b058587988e3f753ed7d317966d75bb0d
Laura Boetcher <g33klaura@gmail.com>
2017-09-27 22:55:42 -0400

    Update auth token (signed in with my Spotify account)

--- assets/javascript/spotify_token.js
+++ assets/javascript/spotify_token.js
@@ -4,6 +4,6 @@
 // VARIABLES ====================
 // 
 
-var authToken = "Bearer BQA7P2irJQJ4LRhZVvlqB5iL2epae2mNDHjjuyMCwr9y5Pf_Ymu0_hYYlM5cJpazmhekqU8SCX_mX6arTFCEig";
+var authToken = "Bearer BQCbGiCNIoZrC9CBBLbNaDYVEb2lHiCdamEg-jfhhyU_KToPv6TiCOwQGoo9hUrJWtjXo7eNAvfkQZGKdfbxVcsAou02XZn1wXvjr7_OftTW38KZlmRb5qhbrRJ_tavNMl6fIVjMXg";
 // ^^Update this as needed until working on own
-// 10:45pm Wed 9/27
+// 10:55pm Wed 9/27
