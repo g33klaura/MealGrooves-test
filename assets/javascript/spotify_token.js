e6c3013148481cd0b380891177d316b0df9f4679
Laura Boetcher <g33klaura@gmail.com>
2017-09-28 18:10:57 -0400

    Update token

--- assets/javascript/spotify_token.js
+++ assets/javascript/spotify_token.js
@@ -4,6 +4,6 @@
 // VARIABLES ====================
 // 
 
-var authToken = "Bearer BQCbGiCNIoZrC9CBBLbNaDYVEb2lHiCdamEg-jfhhyU_KToPv6TiCOwQGoo9hUrJWtjXo7eNAvfkQZGKdfbxVcsAou02XZn1wXvjr7_OftTW38KZlmRb5qhbrRJ_tavNMl6fIVjMXg";
+var authToken = "Bearer BQDgTTMcTvcXfAXJDGfv4Fiok-9jFn7zAod1lNeOpyab1zkm70cLjqw8DjsrP_mBY3QwXyhkMU9Jn3FCvHwk9g";
 // ^^Update this as needed until working on own
 // 10:55pm Wed 9/27
